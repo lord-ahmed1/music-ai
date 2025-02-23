be7b529bd8adbb9062b16e6659ece61a339af6b9
lord-ahmed1 <lordahmed1612@gmail.com>
2025-02-24 01:56:06 +0200

    splitter

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-labels_dir="data/labels/"
+import os
+labels_dir=f"{os.getcwd()}/data/labels/"
+features_dir=f"{os.getcwd()}/data/features/"
+import librosa
+import soundfile as sf
 import re
 
 def turn_clock_to_time(clock):
@@ -17,7 +21,6 @@ def timeline_lyrics_splitter(path):
             end=splited[1]
             start=turn_clock_to_time(start)
             end=turn_clock_to_time(end)
-            print(start,'here')
 
             timeline.append([start,end])
         elif  i!='\n':
@@ -25,5 +28,12 @@ def timeline_lyrics_splitter(path):
             
     return [timeline,words[1:]]
 timeline,words=timeline_lyrics_splitter(labels_dir+"Islamalmighty.vtt")
-print(timeline)
-print(words)
+
+index=3
+print(timeline[index])
+print(words[index])
+audio,sr=librosa.load(features_dir+"Islamalmighty.mp3")
+trimmed=audio[timeline[index][0]*sr:(timeline[index][1]+1)*sr]
+print(trimmed)
+sf.write('output.wav',trimmed,sr,subtype="PCM_24")
+
